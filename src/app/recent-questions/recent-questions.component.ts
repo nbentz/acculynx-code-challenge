5ea326083dd9a2142e065cdb4c7b4158578cc71f
Nicholas Bentzen <bentzen.nich@gmail.com>
2019-06-11 23:36:16 -0500

    generated recent-questions components and service

---
+++ src/app/recent-questions/recent-questions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recent-questions',
+  templateUrl: './recent-questions.component.html',
+  styleUrls: ['./recent-questions.component.css']
+})
+export class RecentQuestionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
