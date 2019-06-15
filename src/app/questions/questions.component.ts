ed224136acb55f260fa8f4349e3eb9a7bb096082
Nicholas Bentzen <bentzen.nich@gmail.com>
2019-06-15 03:03:01 -0500

    started function to keep trackof points

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -11,7 +11,7 @@ export class QuestionsComponent implements OnInit {
   @Input() type: string;
   @Input() title: string;
   questions = [];
-
+  guessedQuestions = [];
 
   constructor( private questionsService: QuestionsService ) { }
 
@@ -20,6 +20,34 @@ export class QuestionsComponent implements OnInit {
     this.questionsService.getQuestions().subscribe( data => {
       this.questions = data.items;
     });
+  }
+
+
+  // scores points, appends answer with timestamp and guesscount.
+  scorePointsAndUpdate(question, answer) {
+    if (answer.is_accepted) {
+      // modal Thats correct, score, etc.
+      alert("Correct");
+    } else {
+      // modal incorrect, score, etc.
+      alert("incorrect");
+    }
+    if (!answer.guess_count) {
+      answer.guess_count = 1;
+    } else {
+      answer.guess_count += 1;
+    }
+    answer.timeStamp = new Date().getTime();
+    const indexAnswer = question.answers.findIndex(x => x.answer_id === answer.answer_id);
+    question.answers[indexAnswer] = answer;
+
+    // this part may not be needed. Maybe for the frontend.
+    const indexQuestion = this.questions.findIndex(x => x.question_id === question.question_id);
+    this.questions[indexQuestion] = question;
+    console.log(this.questions);
+
+    // push object to database, update score if exists.
+
   }
 
   ngOnInit() {
