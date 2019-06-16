5269b23e195d08fd343861ebea0e7d04051acdf6
Nicholas Bentzen <bentzen.nich@gmail.com>
2019-06-16 18:31:27 -0500

    removed two methods that call the api's from the service, and replaced it with one

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -11,26 +11,26 @@ export class QuestionsComponent implements OnInit, OnDestroy {
 
   @Input() type: string;
   @Input() title: string;
+  show = false;
   questions = [];
   guessedQuestions = [];
   subscription: Subscription;
-
-  constructor( public questionsService: QuestionsService ) { }
+  resultStyles = {
+    "correct" : this.show,
+    "incorrect" : !this.show
+  };
+  constructor( public questionsService: QuestionsService ) {}
 
   // gets the most recent questions & answers
-  onSubmitRecentQuestions() {
-    this.questionsService.getQuestionsFromAPI();
+  getAllQuestions() {
+    this.questionsService.getAllQuestions();
   }
-
 
   // scores points, appends answer with timestamp and guesscount.
   scorePointsAndUpdate(question, answer) {
-    console.warn('scorePointsAndUpdate executed');
-    this.questions = this.questionsService.getQuestions();
+    this.questions = this.questionsService.questions;
     answer.is_accepted ? alert('Correct') : alert('Incorrect');
     !answer.guess_count ? answer.guess_count = 1 : answer.guess_count += 1;
-
-    console.log(answer.guess_count);
 
     const indexAnswer = question.answers.findIndex(x => x.answer_id === answer.answer_id);
     question.answers[indexAnswer] = answer;
@@ -39,25 +39,23 @@ export class QuestionsComponent implements OnInit, OnDestroy {
     const indexQuestion = this.questions.findIndex(x => x.question_id === question.question_id);
     this.questions[indexQuestion] = question;
 
+    this.guessedQuestions = this.questionsService.guessedQuestions;
     const indexGuessedQuestion = this.guessedQuestions.findIndex( x => x.question_id === question.question_id);
     indexGuessedQuestion >= 0
       ? this.guessedQuestions[indexGuessedQuestion] = question
-      : this.guessedQuestions = [...this.guessedQuestions, question];
+      : this.guessedQuestions = [question, ...this.guessedQuestions];
 
-    console.log(this.guessedQuestions);
-    this.questionsService.setGuessedQuestions(this.guessedQuestions);
-    console.log(question);
+    this.questionsService.guessedQuestions = this.guessedQuestions;
     this.questionsService.updateQuestion(question);
+  }
+
+  private showAnswerAndGuesses() {
+    this.show = true;
 
   }
 
   ngOnInit() {
-
-    if ( this.type === 'normal') {
-      this.onSubmitRecentQuestions();
-    } else if ( this.type === 'guess') {
-      // Todo: Create method to return recently guessed questions.
-    }
+    this.getAllQuestions();
   }
 
   ngOnDestroy() {
