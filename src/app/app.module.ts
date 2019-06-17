d0bd108b46ee756695b115a5b464bf00c6a60dd1
Nicholas Bentzen <bentzen.nich@gmail.com>
2019-06-16 23:07:36 -0500

    added material models for buttons and dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule, MatExpansionModule } from '@angular/material/';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatTableModule } from '@angular/material/table';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatCardModule } from '@angular/material/card';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { QuestionsComponent } from './questions/questions.component';
 import { QuestionsService } from './questions/questions.service';
+import { ResultsDialogComponent } from './questions/results-dialog/results-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionsComponent
+    QuestionsComponent,
+    ResultsDialogComponent
   ],
   imports: [
     HttpClientModule,
@@ -29,8 +35,12 @@ import { QuestionsService } from './questions/questions.service';
     MatExpansionModule,
     MatDividerModule,
     MatCardModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatDialogModule,
+    MatTableModule,
+    MatButtonModule
   ],
+  entryComponents : [ResultsDialogComponent],
   providers: [QuestionsService],
   bootstrap: [AppComponent]
 })
